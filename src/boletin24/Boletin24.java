84398f0e58963d477ae12984cb59bbd41605cc50
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-03-27 10:28:19 +0100

    Boletin 24

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,14 @@
+
+package boletin24;
+
+/**
+ * @author rafsniper99
+ */
+public class Boletin24 {
+
+    public static void main(String[] args) {
+        Interfaz obx=new Interfaz();
+        obx.crear();
+    }
+    
+}
